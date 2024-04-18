d706d5d1158b08b4975a9dc49375a1c7f13e56e4
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-04-18 21:44:34 +0300

    first task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,28 @@
+import { useEffect } from 'react';
 import styles from './app.module.css';
+import { useState } from 'react';
+
+const route = 'todos';
+const URL = `https://jsonplaceholder.typicode.com/${route}`;
 
 export const App = () => {
-	return <div className={styles.app}>App</div>;
+	const [todos, setTodos] = useState([]);
+
+	useEffect(() => {
+		fetch(URL)
+			.then(loadedData => loadedData.json())
+			.then(loadedTodos => {
+				setTodos(loadedTodos);
+			});
+	}, [URL]);
+
+	return (
+		<div className={styles.app}>
+			{todos.map(({ id, userId, title, completed }) => (
+				<div key={id}>
+					{userId}: {title} - {completed ? 'done' : 'active'}
+				</div>
+			))}
+		</div>
+	);
 };
